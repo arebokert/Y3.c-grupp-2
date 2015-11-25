daeab1c686b1cb1191edcaf2b7a6737e0a2bc95a
Kevin Söderberg <kevso311@student.liu.se>
2015-11-25 14:39:30 +0100

    Adding camera

---
+++ Graphics/Camera.cc
@@ -0,0 +1,10 @@
+#include "Camera.h"
+
+void Camera::update() {
+  
+  //Check where player is in the Array-coordinate system
+  // x = static_cast<int>(Math.floor(player.getX()/32));
+  // y = static_cast<int>(Math.floor(player.getY()/32));
+
+
+}
