9d2049cacccf6711b9bfe58ce4713f6aa6ccf7d3
tomrosenlund <tomro196@liu.student.se>
2015-12-16 15:31:06 +0100

    Load textures for monsters

--- Filehandler/Filehandler.cc
+++ Filehandler/Filehandler.cc
@@ -26,16 +26,14 @@ FileHandler::FileHandler(const string& project) {
   thread t2(&FileHandler::loadBlocks, this);
   thread t3(&FileHandler::loadPlayer, this);
   thread t4(&FileHandler::loadSounds, this);
-  
-  thread t6(&FileHandler::loadWeapons, this);
+  thread t5(&FileHandler::loadMonster, this);
 
   //Vänta på threads med thread.join()
   t1.join();
   t2.join();
   t3.join();
   t4.join();
-  
-  t6.join();
+  t5.join();
 
   //Loading player seperately due to hardware concurrency
   //loadPlayer();
@@ -58,7 +56,6 @@ void FileHandler::loadMap() {
 	cerr << "Error loading file info.txt" << endl;
   }
   else {
-    
     input >> width;
     cout << width << endl; 
     input >> height;
@@ -69,13 +66,8 @@ void FileHandler::loadMap() {
 		input >> map.at(i,j);
       }
   }
- 
-
-  
-  
 }
  
-
 void FileHandler::loadBlocks() {
   //1. Läs in antalet blocks
   //2. Loop
@@ -119,18 +111,39 @@ void FileHandler::loadPlayer() {
     s.append(to_string(i));
     s.append(".png");
     cout << s << endl;
-
     
     if(!player[i].loadFromFile(s))
       cout << "Couldn't load playerTex: " << i << endl;
     else
       cout << "Loaded playerTex: " << i << endl;
     
-
     s = "Data/Player/";
    }
   
 }
+
+void FileHandler::loadMonster() {
+  concurrent.lock();
+  monster = new sf::Texture[6];
+  concurrent.unlock();
+
+  string s{"Data/Monster/"};
+
+  for(int i{0}; i <= 5; i++) {
+
+    s.append(to_string(i));
+    s.append(".png");
+    cout << s << endl;
+   
+    if(!monster[i].loadFromFile(s))
+      cout << "Couldn't load MonsterTex: " << i << endl;
+    else
+      cout << "Loaded MonsterTex: " << i << endl;
+    
+    s = "Data/Monster/";
+   }  
+}
+
 
 void FileHandler::loadSounds() {
   background.setVolume(30);
@@ -147,31 +160,6 @@ void FileHandler::loadSounds() {
   jumpSound.setPitch(0.8);
 }
 
-void FileHandler::loadWeapons() {
-    
-  concurrent.lock();
-  weapons = new sf::Texture[1];
-  concurrent.unlock();
-
-  string s{"Data/Weapons/"};
-
-  for(int i{0}; i < 1; i++) {
-
-    s.append(to_string(i));
-    s.append(".png");
-    cout << s << endl;
-
-    
-    if(!player[i].loadFromFile(s))
-      cout << "Couldn't load weaponTex: " << i << endl;
-    else
-      cout << "Loaded weaponTex: " << i << endl;
-    
-
-    s = "Data/Weapons/";
-   }
-}
-
 
 sf::Texture& FileHandler::getBlock(int i) const {
   return blocks[i];
@@ -180,10 +168,10 @@ sf::Texture& FileHandler::getBlock(int i) const {
 sf::Texture& FileHandler::getPlayer(int i) const {
   return player[i];
 }
-
-sf::Texture& FileHandler::getWeapon(int i) const {
-  return weapons[i];
+sf::Texture& FileHandler::getMonster(int i) const {
+  return monster[i];
 }
+
 
 Matrix FileHandler::getArea(int x1, int y1, int x2, int y2)  {
   return map.getArea(x1,y1,x2,y2);
