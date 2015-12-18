737b79b16dd75e9c29e3f741340d571817dbd6d4
Kevin Söderberg <kevso311@student.liu.se>
2015-12-18 15:00:20 +0100

    Fixed collision

--- Objects/Monster.cc
+++ Objects/Monster.cc
@@ -27,7 +27,8 @@ void Monster::update(Matrix& mat, double delta, Player& play) {
   else
     relPosY += 6;
   
-  if(mat.at(floor(posX/32), floor(posY/32)+1) == 0) {
+  if(mat.at(floor((posX+1)/32), floor((posY+height)/32)) == 0 &&
+     mat.at(floor((posX+width-1)/32), floor((posY+height)/32)) == 0) {
     ySpeed += 600*delta;
   }
   else {
@@ -41,8 +42,10 @@ void Monster::update(Matrix& mat, double delta, Player& play) {
   
 
   //Collision-check on x-axis
-  if(mat.at(floor(posX/32)+1, floor(posY/32)) != 0 && xSpeed > 0 ||
-     mat.at(floor(posX/32), floor(posY/32)) != 0 && xSpeed < 0) {
+  if(mat.at(floor((posX+width)/32), floor(posY/32)) != 0 && xSpeed > 0 ||
+     mat.at(floor((posX+width)/32), floor((posY+height-1)/32)) != 0 && xSpeed > 0 ||
+     mat.at(floor(posX/32), floor(posY/32)) != 0 && xSpeed < 0 ||
+     mat.at(floor(posX/32), floor((posY+height-1)/32)) != 0 && xSpeed < 0) {
     xSpeed = 0;
     goRight = !goRight;
   }
